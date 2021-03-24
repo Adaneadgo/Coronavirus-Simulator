5dd0cc893c37d6a09e40b152fa4c529ebb0a1572
Elie Bracha <eliebrachapro@gmail.com>
2021-03-24 09:44:34 +0100

    Add Map and Settlement (enum problem)

---
+++ Assignement_1/src/Country/Settlement.java
@@ -0,0 +1,18 @@
+package Country;
+
+import Location.Location;
+import Population.Person;
+
+import java.util.List;
+
+public class Settlement {
+
+    private String m_name;
+    private Location location;
+    List<Person> people;
+    Map.RamzorColor ramzorColor;
+
+
+
+
+}
--- Assignement_1/src/Population/Person.java
+++ Assignement_1/src/Population/Person.java
@@ -1,6 +1,10 @@
 package Population;
 
+import Location.Location;
+
 public class Person {
     private int m_age;
+    private Location location;
+    Settlement settlement;
 
 }
