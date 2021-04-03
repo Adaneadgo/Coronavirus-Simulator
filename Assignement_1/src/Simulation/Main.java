bc461c3d24d56d582e9d16929cff7ad4465b044f
Adane <adaneadgo1@gmail.com>
2021-04-04 02:52:28 +0300

    encapsultion and stuff

--- Assignement_1/src/Simulation/Main.java
+++ Assignement_1/src/Simulation/Main.java
@@ -1,31 +1,15 @@
 package Simulation;
 
-import Location.*;
-import Virus.*;
-
-import java.util.Random;
-
-import Country.*;
-import Population.*;
-import IO.*;
+import IO.SimulationFile;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        SimulationFile SF = new SimulationFile("C:\\Users\\adane\\Desktop\\myCodesGit\\Collage\\AdvancedOOP\\Assigment1_OOP_Java\\Assignement_1\\src\\Simulation\\Test.txt");
-
-        // Step 1
-        SF.loadSimulation();
-        //Step 2
-        SF.initialSimulation();
-        //
-
-        System.out.println(SF);
-
-
-
-
+        SimulationFile mySimulation = new SimulationFile("inputFile.txt");
+        mySimulation.loadSimulation();
+        mySimulation.initialSimulation();
+        System.out.println(mySimulation);
 
     }
 
