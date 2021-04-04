295a7ea825724c4249c881a734ce2e1d52e204c3
Adane <adaneadgo1@gmail.com>
2021-04-04 11:51:56 +0300

    finish EX

--- Assignement_1/src/Simulation/Main.java
+++ Assignement_1/src/Simulation/Main.java
@@ -3,13 +3,17 @@ package Simulation;
 import IO.SimulationFile;
 
 
+
 public class Main {
     public static void main(String[] args) throws Exception {
 
         SimulationFile mySimulation = new SimulationFile("inputFile.txt");
-        mySimulation.loadSimulation();
-        mySimulation.initialSimulation();
-        System.out.println(mySimulation);
+        mySimulation.loadSimulation(); // Step 1
+        mySimulation.initialSimulation(); // Step 2
+        mySimulation.simulateSimulation(); // Step 3
+        System.out.println(mySimulation); 
+
+    
 
     }
 
