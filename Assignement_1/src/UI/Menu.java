113b739bf8ed6eae9fca978ae74126d67bb5d274
Adane <adaneadgo1@gmail.com>
2021-04-27 15:43:37 +0300

    resave changes

---
+++ Assignement_1/src/UI/Menu.java
@@ -0,0 +1,38 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Menu {
+
+    public Menu(){
+        
+        JFrame frame = new JFrame("main window");
+        BorderLayout myBorderLayout = new BorderLayout();
+        myBorderLayout.setHgap(20);
+        myBorderLayout.setVgap(20);
+        frame.setLayout(myBorderLayout);
+
+
+
+
+
+
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+
+
+
+    }
+
+
+    
+   
+
+
+    
+    
+}
